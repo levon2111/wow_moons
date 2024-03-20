2aad13a03dfabd64ae65b26cf8134e13666336fe
levonyeghiazaryan <levon2111@gmail.com>
2024-03-20 11:24:04 +0400

    add all pages

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "@/app/includes/header";
 import Footer from "@/app/includes/footer";
 import Link from "next/link";
-
+import 'tailwindcss/tailwind.css';
 export default function Home() {
     return (
         <>
